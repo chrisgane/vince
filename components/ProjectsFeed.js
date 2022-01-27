2b938cafe870255d2d83786cc2b4844266ad0b22
Daniel Stopka <stopka.d@gmail.com>
2022-01-27 18:40:07 +0100

    fix: fade up animation not working during tab switching

--- components/ProjectsFeed.js
+++ components/ProjectsFeed.js
@@ -1,27 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import Link from "next/link";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import AOS from "aos";
 
 const ProjectsFeed = ({ projects }) => {
+    const [projectsState, setProjects] = useState([]);
+
     useEffect(() => {
-        AOS.init();
-    }, []);
+        // animation fix
+        setProjects([]);
+        setTimeout(() => {
+            setProjects(projects);
+        }, 0);
+    }, [projects]);
+
     return (
-        <div className=" mt-10">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-y-16  px-6 md:px-12 lg:px-40">
-                {projects.map((project) => (
+        <div className="mt-10 min-h-screen">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-y-16 px-6 md:px-12 lg:px-40">
+                {projectsState.map((project) => (
                     <Link
                         key={project.id}
                         id="projectItem"
                         href={`/work/${project.slug}`}
                     >
-                        <a>
-                            <div
-                                data-aos="fade-up"
-                                className=" flex flex-col justify-center items-center text-center"
-                            >
+                        <a data-aos="fade-up">
+                            <div className="flex flex-col justify-center items-center text-center">
                                 <div className="overflow-hidden">
                                     <motion.img
                                         whileHover={{ scale: 1.03 }}

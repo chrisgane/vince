e9d09cccd83c4345acd521039e4569f09ec67a39
Daniel Stopka <stopka.d@gmail.com>
2022-01-28 18:22:53 +0100

    fix: tailwind fix

--- components/reusable/grid/Grid.jsx
+++ components/reusable/grid/Grid.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 
 const Grid = ({ children, className = "", cols = 3 }) => {
+    let gridCols = "lg:grid-cols-1";
+
+    switch (cols) {
+        case 2:
+            gridCols = "lg:grid-cols-2";
+            break;
+        case 3:
+            gridCols = "lg:grid-cols-3";
+            break;
+        case 4:
+            gridCols = "lg:grid-cols-4";
+            break;
+    }
+
     return (
         <div
-            className={`grid grid-cols-1 w-full md:grid-cols-2 lg:grid-cols-${cols} gap-16 px-6 md:px-1 ${className}`}
+            className={`grid grid-cols-1 w-full md:grid-cols-2 ${gridCols} gap-16 px-6 md:px-1 ${className}`}
         >
             {children}
         </div>

4230fa8d85db02ef42d11324dfefffe8ea88b494
Daniel Stopka <stopka.d@gmail.com>
2022-02-01 15:58:25 +0100

    feat: new image on work pages

--- components/ProjectsFeed.js
+++ components/ProjectsFeed.js
@@ -1,8 +1,6 @@
-import { useEffect, useState } from "react";
-
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import { motion } from "framer-motion";
-import AOS from "aos";
+import Image from "next/image";
 
 const ProjectsFeed = ({ projects }) => {
     const [projectsState, setProjects] = useState([]);
@@ -18,7 +16,7 @@ const ProjectsFeed = ({ projects }) => {
     return (
         <div className="mt-10 min-h-screen">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-16 px-6 md:px-1">
-                {projectsState.map((project) => (
+                {projectsState.map((project, index) => (
                     <Link
                         key={project.id}
                         id="projectItem"
@@ -27,10 +25,19 @@ const ProjectsFeed = ({ projects }) => {
                         <a data-aos="fade-up">
                             <div className="flex flex-col justify-center items-center text-center">
                                 <div className="overflow-hidden">
-                                    <motion.img
-                                        whileHover={{ scale: 1.03 }}
-                                        src={project.acf.image}
-                                        className="w-full h-full"
+                                    <Image
+                                        {...(index !== 0 && {
+                                            placeholder: "blur",
+                                            blurDataURL:
+                                                project.acf.image.sizes
+                                                    .thumbnail,
+                                        })}
+                                        src={project.acf.image.url}
+                                        className="w-full h-full img-scale"
+                                        width={project.acf.image.width}
+                                        height={project.acf.image.height}
+                                        alt={project.acf.image.alt}
+                                        {...(index === 0 && { priority: true })}
                                     />
                                 </div>
                                 <h3 className="font-normal text-gray-600 uppercase tracking-widest px-2 mt-6 mb-3">

07933c03d4a9bd4477601449837731f0624ef355
Daniel Stopka <stopka.d@gmail.com>
2022-02-01 16:50:26 +0100

    feat: add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -23,6 +23,31 @@ class MyDocument extends Document {
                         href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;600;800&family=Poppins:ital,wght@0,300;0,400;0,700;0,800;1,600&display=swap"
                         rel="stylesheet"
                     ></link>
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="180x180"
+                        href="/apple-touch-icon.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="32x32"
+                        href="/favicon-32x32.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="16x16"
+                        href="/favicon-16x16.png"
+                    />
+                    <link rel="manifest" href="/site.webmanifest" />
+                    <link
+                        rel="mask-icon"
+                        href="/safari-pinned-tab.svg"
+                        color="#000000"
+                    />
+                    <meta name="msapplication-TileColor" content="#da532c" />
+                    <meta name="theme-color" content="#ffffff"></meta>
                 </Head>
                 <body>
                     <Main />

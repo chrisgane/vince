bf0b9bd6a7ca41ee42735e958aaa0c5de013dc7b
Daniel Stopka <stopka.d@gmail.com>
2022-02-01 15:33:53 +0100

    feat: contact page refactor

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,4 @@
 import React from "react";
-import useSWR from "swr";
 import Obfuscate from "react-obfuscate";
 import { motion } from "framer-motion";
 import Container from "../components/reusable/Container";
@@ -10,11 +9,6 @@ import {
 } from "../utils/animation-settings";
 
 const Contact = () => {
-    const fetcher = (url) => fetch(url).then((res) => res.json());
-    const { data, error } = useSWR(
-        "https://stupefied-antonelli.136-244-69-22.plesk.page/index.php/wp-json/wp/v2/pages/132",
-        fetcher,
-    );
     const socials = [
         { name: "Facebook", url: "https://www.facebook.com/madebyvince/" },
         { name: "Twitter", url: "https://twitter.com/madebyvince" },
@@ -23,68 +17,66 @@ const Contact = () => {
 
     return (
         <>
-            {data && (
-                <motion.div
-                    initial={fadeRightInitial}
-                    animate={fadeRightAnimate}
-                    exit={fadeRightExit}
-                >
-                    <Container className="pt-0 md:pt-16  px-4 pb-48">
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-4xl mx-auto ">
-                            <div className="">
-                                <div className="text-gray-400 font-light text-sm  tracking-wide mb-6">
-                                    CONTACT US
-                                </div>
-                                <div className="text-2xl font-bold text-gray-800">
-                                    <div className="my-2">
-                                        <Obfuscate tel="+44 (0)20 7470 9229" />
-                                    </div>
-                                    <Obfuscate email="production@vince.co.uk" />
-                                </div>
+            <motion.div
+                initial={fadeRightInitial}
+                animate={fadeRightAnimate}
+                exit={fadeRightExit}
+            >
+                <Container className="pt-0 md:pt-16  px-4 pb-48">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-4xl mx-auto ">
+                        <div className="">
+                            <div className="text-gray-400 font-light text-sm  tracking-wide mb-6">
+                                CONTACT US
                             </div>
-                            <div className="my-10 md:my-0">
-                                <div className="text-gray-400 font-light text-sm  tracking-wide mb-6">
-                                    FIND US
+                            <div className="text-2xl font-bold text-gray-800">
+                                <div className="my-2">
+                                    <Obfuscate tel="+44 (0)20 7470 9229" />
                                 </div>
-                                <div className="text-2xl font-bold text-gray-800">
-                                    <div className="">
-                                        VINCE
-                                        <br />
-                                        1st floor 40
-                                        <br />
-                                        Drury Lane
-                                        <br />
-                                        London, WC2B 5RR.
-                                    </div>
-                                </div>
+                                <Obfuscate email="production@vince.co.uk" />
                             </div>
-                            <div className="">
-                                <div className="text-gray-400 font-light text-sm  tracking-wide mb-6">
-                                    FOLLOW US
-                                </div>
-                                <div className="text-2xl font-bold text-gray-800">
-                                    <ul className="flex items-center my-6 lg:my-0">
-                                        {socials.map((item) => (
-                                            <li
-                                                key={item.name}
-                                                className="mr-5 hover:text-gray-300"
-                                            >
-                                                <a
-                                                    href={item.url}
-                                                    target="_blank"
-                                                    rel="noopener noreferrer"
-                                                >
-                                                    {item.name}
-                                                </a>
-                                            </li>
-                                        ))}
-                                    </ul>
+                        </div>
+                        <div className="my-10 md:my-0">
+                            <div className="text-gray-400 font-light text-sm  tracking-wide mb-6">
+                                FIND US
+                            </div>
+                            <div className="text-2xl font-bold text-gray-800">
+                                <div className="">
+                                    VINCE
+                                    <br />
+                                    1st floor 40
+                                    <br />
+                                    Drury Lane
+                                    <br />
+                                    London, WC2B 5RR.
                                 </div>
                             </div>
                         </div>
-                    </Container>
-                </motion.div>
-            )}
+                        <div className="">
+                            <div className="text-gray-400 font-light text-sm  tracking-wide mb-6">
+                                FOLLOW US
+                            </div>
+                            <div className="text-2xl font-bold text-gray-800">
+                                <ul className="flex items-center my-6 lg:my-0">
+                                    {socials.map((item) => (
+                                        <li
+                                            key={item.name}
+                                            className="mr-5 hover:text-gray-300"
+                                        >
+                                            <a
+                                                href={item.url}
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                            >
+                                                {item.name}
+                                            </a>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </Container>
+            </motion.div>
         </>
     );
 };

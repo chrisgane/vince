bd9ac7ab8bc6de5d11aeb32c74903c1d0c705aa5
Daniel Stopka <stopka.d@gmail.com>
2022-02-07 15:43:13 +0100

    feat: remove current project from rest projects

--- components/main/rest-projects/RestProjects.jsx
+++ components/main/rest-projects/RestProjects.jsx
@@ -3,7 +3,7 @@ import ProjectCard from "../../reusable/project-card/ProjectCard";
 
 const RestProjects = ({ posts }) => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-16 px-6 md:px-1 mt-16">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-16 px-6 md:px-1 mt-8 md:mt-16">
             {posts.map((project) => (
                 <ProjectCard key={project.id} project={project} />
             ))}
